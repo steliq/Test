ce3b7d93a8a5a37e5f2cae6592aa658a4f05244c
steliq <steliq@github.com>
2019-07-13 13:02:11 +0300

    Am updatat codul

--- Test2/src/Proba.java
+++ Test2/src/Proba.java
@@ -4,6 +4,8 @@ public class Proba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Proba");
+		
+		System.out.println("Am creat un branch nou");
 	}
 
 }
